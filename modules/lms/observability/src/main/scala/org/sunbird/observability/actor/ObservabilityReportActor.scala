3b098b2e5db0d80902c6cd95866dad5bc7534c8a
chethann007 <gchethan7602@gmail.com>
2026-03-11 06:22:27 +0530

    refactor: Update ObservabilityReportActor to handle exceptions and improve error logging, and adjust related routing configurations.

--- modules/lms/observability/src/main/scala/org/sunbird/observability/actor/ObservabilityReportActor.scala
+++ modules/lms/observability/src/main/scala/org/sunbird/observability/actor/ObservabilityReportActor.scala
@@ -1,7 +1,9 @@
 package org.sunbird.observability.actor
 
 import org.sunbird.actor.core.BaseActor
+import org.sunbird.exception.ProjectCommonException
 import org.sunbird.logging.LoggerUtil
+import org.sunbird.message.ResponseCode
 import org.sunbird.observability.service.{ObservabilityReportService, ObservabilityReportServiceImpl}
 import org.sunbird.request.Request
 
@@ -14,24 +16,36 @@ import org.sunbird.request.Request
  *
  * The service dependency is constructor-injectable for unit testing.
  */
-class ObservabilityReportActor(
-    service: ObservabilityReportService = ObservabilityReportActor.sharedService
-) extends BaseActor {
+class ObservabilityReportActor(service: ObservabilityReportService) extends BaseActor {
+
+  def this() = this(ObservabilityReportActor.sharedService)
 
   private val log = new LoggerUtil(classOf[ObservabilityReportActor])
 
   override def onReceive(request: Request): Unit = {
-    request.getOperation match {
-      case "generateReport" =>
-        log.info(request.getRequestContext, "ObservabilityReportActor: handling generateReport")
-        sender().tell(service.generateReport(request), self)
+    try {
+      request.getOperation match {
+        case "generateReport" =>
+          log.info(request.getRequestContext, "ObservabilityReportActor: handling generateReport")
+          sender().tell(service.generateReport(request), self)
 
-      case "listReports" =>
-        log.info(request.getRequestContext, "ObservabilityReportActor: handling listReports")
-        sender().tell(service.listReports(request), self)
+        case "listReports" =>
+          log.info(request.getRequestContext, "ObservabilityReportActor: handling listReports")
+          sender().tell(service.listReports(request), self)
 
-      case _ =>
-        onReceiveUnsupportedOperation(request.getOperation)
+        case _ =>
+          onReceiveUnsupportedOperation(request.getOperation)
+      }
+    } catch {
+      case ex: Exception => throw ex
+      case t: Throwable =>
+        log.error(request.getRequestContext, "ObservabilityReportActor: unexpected error in onReceive", t)
+        val pce = new ProjectCommonException(
+          ResponseCode.serverError.getErrorCode,
+          "Internal server error: " + t.getMessage,
+          ResponseCode.SERVER_ERROR.getResponseCode
+        )
+        sender().tell(pce, self)
     }
   }
 }
